2098a218a46de2ec00b52dc2d641152f6ddc21e3
braccinocorto <braccinocorto@gmail.com>
2023-06-13 14:53:16 +0200

    date for datetime

--- wk3/orchestrate.py
+++ wk3/orchestrate.py
@@ -9,6 +9,7 @@ from sklearn.metrics import mean_squared_error
 import mlflow
 import xgboost as xgb
 from prefect import flow, task
+from datetime import date
 
 
 @task(retries=3, retry_delay_seconds=2, name="Read taxi data")
