42196f12bcd0746debe257b0201bd1a20e9eb221
braccinocorto <braccinocorto@gmail.com>
2023-07-17 18:23:02 +0200

    wk6 homework

---
+++ wk6/tests/test_batch.py
@@ -0,0 +1,37 @@
+
+import batch_refact
+import pandas as pd
+
+from datetime import datetime
+
+def dt(hour, minute, second=0):
+    return datetime(2022, 1, 1, hour, minute, second)
+
+data = [
+    (None, None, dt(1, 2), dt(1, 10)),
+    (1, None, dt(1, 2), dt(1, 10)),
+    (1, 2, dt(2, 2), dt(2, 3)),
+    (None, 1, dt(1, 2, 0), dt(1, 2, 50)),
+    (2, 3, dt(1, 2, 0), dt(1, 2, 59)),
+    (3, 4, dt(1, 2, 0), dt(2, 2, 1)),     
+]
+
+categorical = ['PULocationID', 'DOLocationID']
+columns = ['PULocationID', 'DOLocationID', 'tpep_pickup_datetime', 'tpep_dropoff_datetime']
+df = pd.DataFrame(data, columns=columns)
+
+def prepare_data(df, categorical):
+    df['duration'] = df.tpep_dropoff_datetime - df.tpep_pickup_datetime
+    df['duration'] = df.duration.dt.total_seconds() / 60
+    df = df[(df.duration >= 1) & (df.duration <= 60)].copy()
+    df[categorical] = df[categorical].fillna(-1).astype('int').astype('str')
+    return df
+
+expected_lines = 3
+ret_df = prepare_data(df, categorical)
+actual_lines = ret_df.shape[0]
+
+print(ret_df.shape)
+
+assert actual_lines == expected_lines
+
